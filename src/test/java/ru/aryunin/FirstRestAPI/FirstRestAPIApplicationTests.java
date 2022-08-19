9d7f3327f0ff5f6346e60df78d6d2c4a0365f32d
aryunin <im.aryunin@gmail.com>
2022-08-19 18:14:28 +0300

    Initial commit

---
+++ src/test/java/ru/aryunin/FirstRestAPI/FirstRestAPIApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aryunin.FirstRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstRestAPIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
