fc53dce626437608603fd0e3fb5ae2826b87dde0
aryunin <im.aryunin@gmail.com>
2022-08-19 18:49:40 +0300

    SensorRepository added

---
+++ src/main/java/ru/aryunin/FirstRestAPI/repositories/SensorRepository.java
@@ -0,0 +1,12 @@
+package ru.aryunin.FirstRestAPI.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.aryunin.FirstRestAPI.models.Sensor;
+
+import java.util.Optional;
+
+@Repository
+public interface SensorRepository extends JpaRepository<Sensor, Integer> {
+    Optional<Sensor> findByName(String name);
+}
