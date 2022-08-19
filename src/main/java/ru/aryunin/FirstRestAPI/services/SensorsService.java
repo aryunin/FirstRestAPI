9e73d608d348a23d13deb48c95f5bf02728ae48a
aryunin <im.aryunin@gmail.com>
2022-08-19 19:12:42 +0300

    Constructor replaced by Lombok annotation

--- src/main/java/ru/aryunin/FirstRestAPI/services/SensorsService.java
+++ src/main/java/ru/aryunin/FirstRestAPI/services/SensorsService.java
@@ -1,5 +1,6 @@
 package ru.aryunin.FirstRestAPI.services;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.aryunin.FirstRestAPI.models.Sensor;
 import ru.aryunin.FirstRestAPI.repositories.SensorRepository;
@@ -7,14 +8,15 @@ import ru.aryunin.FirstRestAPI.repositories.SensorRepository;
 import java.util.Optional;
 
 @Service
+@AllArgsConstructor
 public class SensorsService {
     private final SensorRepository sensorRepository;
 
-    public SensorsService(SensorRepository sensorRepository) {
-        this.sensorRepository = sensorRepository;
-    }
-
     public Optional<Sensor> findByName(String name) {
         return sensorRepository.findByName(name);
+    }
+
+    public void save(Sensor sensor) {
+        sensorRepository.save(sensor);
     }
 }
