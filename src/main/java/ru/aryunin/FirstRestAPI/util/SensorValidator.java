10f2febf3b1a716e17b5f4925d0948b77e45d48d
aryunin <im.aryunin@gmail.com>
2022-08-19 18:51:07 +0300

    SensorValidation with validation of the sensor's existence in the DB

---
+++ src/main/java/ru/aryunin/FirstRestAPI/util/SensorValidator.java
@@ -0,0 +1,26 @@
+package ru.aryunin.FirstRestAPI.util;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import ru.aryunin.FirstRestAPI.models.Sensor;
+import ru.aryunin.FirstRestAPI.services.SensorsService;
+
+public class SensorValidator implements Validator {
+    private final SensorsService sensorsService;
+
+    public SensorValidator(SensorsService sensorsService) {
+        this.sensorsService = sensorsService;
+    }
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return Sensor.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        Sensor sensor = (Sensor) o;
+
+        if(sensorsService.findByName(sensor.getName()).isEmpty()) errors.rejectValue("name", "", "A sensor with the same name already exists!");
+    }
+}
