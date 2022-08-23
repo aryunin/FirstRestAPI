a115f62c310a730224644f8e703975613dff3bc3
Artem Yunin <im.aryunin@gmail.com>
2022-08-23 09:35:18 +0300

    MeasurementDTO added

---
+++ src/main/java/ru/aryunin/FirstRestAPI/DTO/MeasurementDTO.java
@@ -0,0 +1,19 @@
+package ru.aryunin.FirstRestAPI.DTO;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class MeasurementDTO {
+    @Min(value = -100, message = "The value should be greater than -100!")
+    @Max(value = 100, message = "The value should be less than 100!")
+    private float value;
+    private Boolean raining;
+    private SensorDTO sensorDTO;
+}
