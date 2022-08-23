1009dffc4c2a0b9286fd33c35ffac0eefc4e09ec
Artem Yunin <im.aryunin@gmail.com>
2022-08-23 12:22:00 +0300

    findAllJoin method added

--- src/main/java/ru/aryunin/FirstRestAPI/repositories/MeasurementRepository.java
+++ src/main/java/ru/aryunin/FirstRestAPI/repositories/MeasurementRepository.java
@@ -1,9 +1,14 @@
 package ru.aryunin.FirstRestAPI.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.aryunin.FirstRestAPI.models.Measurement;
 
+import java.util.List;
+
 @Repository
 public interface MeasurementRepository extends JpaRepository<Measurement, Integer> {
+    @Query(value = "SELECT m FROM Measurement m LEFT JOIN FETCH m.sensor s")
+    List<Measurement> findAllJoin();
 }
